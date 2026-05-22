3111b6da565954e1f68a86691f62ec3e626457c1
shj321 <9051863@qq.com>
2026-05-22 21:36:47 +0800

    fix: use IceServer constructor instead of builder for compatibility

--- app/src/main/java/com/p2pchat/app/p2p/P2PConnectionManager.java
+++ app/src/main/java/com/p2pchat/app/p2p/P2PConnectionManager.java
@@ -59,12 +59,9 @@ public class P2PConnectionManager implements PeerConnectionObserverBase {
             PeerConnection.IceServer.builder("stun:stun.l.google.com:19302").createIceServer(),
             PeerConnection.IceServer.builder("stun:stun1.l.google.com:19302").createIceServer(),
             // TURN (Open Relay 免费公共 TURN)
-            PeerConnection.IceServer.builder("turn:openrelay.metered.ca:80")
-                .setUsername("openrelayproject").setPassword("openrelayproject").createIceServer(),
-            PeerConnection.IceServer.builder("turn:openrelay.metered.ca:443")
-                .setUsername("openrelayproject").setPassword("openrelayproject").createIceServer(),
-            PeerConnection.IceServer.builder("turns:openrelay.metered.ca:443")
-                .setUsername("openrelayproject").setPassword("openrelayproject").createIceServer()
+            new PeerConnection.IceServer("turn:openrelay.metered.ca:80", "openrelayproject", "openrelayproject"),
+            new PeerConnection.IceServer("turn:openrelay.metered.ca:443", "openrelayproject", "openrelayproject"),
+            new PeerConnection.IceServer("turns:openrelay.metered.ca:443", "openrelayproject", "openrelayproject")
         );
 
         initWebRTC();
